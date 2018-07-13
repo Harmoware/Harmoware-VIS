403a324ba9991ae6639c50f1c004bc65f22d5bf8
DESKTOP-5CBUTHR\yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2018-07-13 16:07:25 +0900

    visualize-sample-nonmapサンプルにi18nextで国際化対応

--- examples/visualize-sample-nonmap/index.js
+++ examples/visualize-sample-nonmap/index.js
@@ -2,14 +2,18 @@ import { render } from 'react-dom';
 import { reducer } from 'harmoware-vis';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { I18nextProvider } from 'react-i18next';
 import React from 'react';
 import App from './containers';
+import i18n from './I18n';
 
 const store = createStore(combineReducers({ ...reducer }));
 
 render(
   <Provider store={store}>
-    <App />
+    <I18nextProvider i18n={i18n} >
+      <App />
+    </I18nextProvider>
   </Provider>,
   document.getElementById('app')
 );
