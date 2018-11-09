652fe9d6e9279e2db7552404f81a831446049585
DESKTOP-5CBUTHR\yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2018-11-09 14:08:36 +0900

    js delete

--- src/constants/settings.js
+++
@@ -1,6 +0,0 @@
-export var COLOR1 = [0, 255, 0]; // バスアイコン基本色 GREEN
-export var COLOR2 = [255, 255, 255]; // バスアイコン出発色 WHITE
-export var COLOR3 = [0, 255, 255]; // バスアイコン選択色 CYAN
-export var COLOR4 = [166, 89, 166]; // バス停アイコン基本色 DARKMAGENTA
-export var COLOR5 = [255, 0, 255]; // 未使用色 MAGENTA
-//# sourceMappingURL=settings.js.map
