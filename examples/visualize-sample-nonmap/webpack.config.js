ad99c45bd5715201ac3331cbb14940f480a4c700
Hori Godai <ess_president@me.com>
2018-11-13 14:03:16 +0900

    tsc commandを追加

--- examples/visualize-sample-nonmap/webpack.config.js
+++ examples/visualize-sample-nonmap/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       // Compile ES2015 using buble
       test: /\.ts|\.tsx|\.js|\.jsx$/,
       loader: 'babel-loader',
-      include: [resolve(__dirname), resolve(__dirname, '../../src'), resolve(__dirname, './node_modules/mapbox-gl/js/')],
+      include: [resolve(__dirname), resolve(__dirname, './src'), resolve(__dirname, './node_modules/mapbox-gl/js/')],
       options: {
         presets: [
           '@babel/env',
