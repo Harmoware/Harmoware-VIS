ef2aa144522142223b749c1a57612f2059fd4581
Hori Godai <ess_president@me.com>
2019-01-23 11:50:01 +0900

    fix dependencies package.json

--- types/src/constants/settings.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const COLOR1: number[];
-export declare const COLOR2: number[];
-export declare const COLOR3: number[];
-export declare const COLOR4: number[];
-export declare const COLOR5: number[];
