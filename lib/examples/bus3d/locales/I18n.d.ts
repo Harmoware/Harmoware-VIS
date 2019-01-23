22d5cce48f9abbd389019840240bfa48440d3e75
Hori Godai <ess_president@me.com>
2019-01-23 11:58:58 +0900

    remove examples types

--- lib/examples/bus3d/locales/I18n.d.ts
+++
@@ -1,3 +0,0 @@
-import * as i18next from 'i18next';
-declare const i18n: i18next.i18n;
-export default i18n;
--- lib/examples/visualize-sample/containers/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: import("react-redux").ConnectedComponentClass<any, Pick<{}, never>>;
-export default _default;
