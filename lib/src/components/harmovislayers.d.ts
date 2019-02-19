a751798cc44c8e4a4269fe13c333b9dd8dc44a09
Hori Godai <ess_president@me.com>
2019-02-19 10:15:22 +0900

    tsc command

--- lib/src/components/harmovislayers.d.ts
+++ lib/src/components/harmovislayers.d.ts
@@ -8,10 +8,12 @@ interface Props {
     actions: ActionTypes;
     onViewportChange?(viewport: Viewport): void;
     layers: Layer[];
+    mapGlComponents?: any;
 }
 export default class HarmoVisLayers extends React.Component<Props> {
     static defaultProps: {
         mapStyle: string;
+        mapGlComponents: any;
     };
     componentDidMount(): void;
     initialize(gl: WebGLRenderingContext): void;
