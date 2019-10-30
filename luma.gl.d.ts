161a91395b903cd53c4f406c579405191cd292f0
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2019-10-30 09:55:12 +0900

    material sample

--- luma.gl.d.ts
+++ luma.gl.d.ts
@@ -1,23 +1,5 @@
-declare module "luma.gl" {
-  interface Uniforms {}
-  class Geometry {
-    constructor(opts: { drawMode: number, positions: Float32Array });
+declare module "@luma.gl/core" {
+  class PhongMaterial {
+    constructor(opts: {ambient: number, diffuse: number, shininess: number, specularColor: number[]});
   }
-  class Model {
-    constructor(gl: WebGLRenderingContext, opts: object);
-    render(uniforms: Uniforms): this;
-    setUniforms(uniforms: Uniforms): this;
-  }
-  class CubeGeometry extends Geometry {
-    constructor(opts?: { drawMode: number, positions: Float32Array });
-  }
-  const picking: { name: 'picking' }
-  function registerShaderModules(shaderModuleList: (typeof picking)[], { ignoreMultipleRegistrations }?: {
-    ignoreMultipleRegistrations?: boolean;
-  }): void
-}
-
-declare module "@luma.gl/constants" {
-  const GL: { UNSIGNED_BYTE: number, TRIANGLE_STRIP: number, TRIANGLES: number }
-  export default GL;
 }
--- src/layers/cubeicon-layer/index.ts
+++ src/layers/cubeicon-layer/index.ts
@@ -1,4 +1,5 @@
 import { LayerProps, GridCellLayer } from 'deck.gl';
+import { PhongMaterial } from '@luma.gl/core';
 
 const DEFAULT_COLOR = [255, 255, 255, 255];
 
@@ -17,14 +18,23 @@ interface Props extends LayerProps {
 export default class CubeiconLayer extends GridCellLayer<Props> {
 
   constructor(props: Props) {
-    super(Object.assign({}, props ,{getElevation: props.getHeight, offset: [0,0]}) );
+    const { getColor, getFillColor, getLineColor, ...otherProps } = props;
+    super(Object.assign({}, otherProps ,{
+      getFillColor: getFillColor || getColor,
+      getLineColor: getLineColor || getColor,
+      getElevation: props.getHeight,
+      offset: [0,0]}) );
   }
 
   static defaultProps = {
     getHeight: (x: any) => x.height, // height:height
-    getFillColor: (x: any) => x.color || DEFAULT_COLOR,
-    getLineColor: (x: any) => x.color || DEFAULT_COLOR,
-    getColor: {deprecatedFor: ['getFillColor', 'getLineColor']}
+    getColor: (x: any) => x.color || DEFAULT_COLOR,
+    material: new PhongMaterial({
+      ambient: 0.4,
+      diffuse: 0.6,
+      shininess: 32,
+      specularColor: [30, 30, 30]
+    })
   };
 
   static layerName = 'CubeiconLayer';
