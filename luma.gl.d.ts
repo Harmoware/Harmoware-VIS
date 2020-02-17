f4e450d59917f95412976851ca34f9d3f31d0503
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-02-17 15:22:01 +0900

    Improved array and object generation.

--- luma.gl.d.ts
+++ luma.gl.d.ts
@@ -1,22 +1,3 @@
-declare module "luma.gl" {
-  class IcoSphereGeometry {
-    constructor(props?: Object);
-  }
-  class CubeGeometry {
-    constructor(props?: Object);
-  }
-}
-
-declare module "@luma.gl/core" {
-  class PhongMaterial {
-    constructor(opts: {ambient: number, diffuse: number, shininess: number, specularColor: number[]});
-  }
-}
-
-declare module "@luma.gl/addons" {
-  const GLTFScenegraphLoader:any
-}
-
-declare module "@loaders.gl/core" {
-  const registerLoaders:any
-}
+declare module "luma.gl" ;
+declare module "@luma.gl/addons" ;
+declare module "@loaders.gl/core" ;
