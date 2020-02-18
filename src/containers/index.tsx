f892640602a7df8cbaeadde4013d900220adb061
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-02-18 16:46:30 +0900

    Minor improvements

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { BasedProps as Props } from '../types';
 
-export default class Root<P extends Props, S = {}> extends React.Component<P, S> {
+export default class Container<P extends Props, S = {}> extends React.Component<P, S> {
 
   constructor(props: P) {
     super(props);
