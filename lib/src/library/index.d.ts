f2a9a2b5a96a0930880311ab6d5e20f13f31ff7e
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-04-23 12:00:53 +0900

    build:lib

--- lib/src/library/index.d.ts
+++ lib/src/library/index.d.ts
@@ -1,5 +1,5 @@
 import * as Actions from '../actions';
-import { AnalyzedBaseData, InnerProps, RoutePaths, MovesbaseFile, Movesbase, MovedData, DepotsData, ClickedObject, EventInfo } from '../types';
+import { AnalyzedBaseData, InnerProps, RoutePaths, IconDesignation, MovesbaseFile, Movesbase, MovedData, DepotsData, ClickedObject, EventInfo } from '../types';
 export declare const getContainerProp: <P>(state: P) => P;
 export declare const calcLoopTime: (timeLength: number, secperhour: number) => number;
 export declare const analyzeMovesBase: (inputData: MovesbaseFile | Movesbase[]) => AnalyzedBaseData;
@@ -9,7 +9,7 @@ export interface pickParams {
     mode: string;
     info: EventInfo;
 }
-export declare const onHoverClick: (pickParams: pickParams, getRouteColor: Function, getRouteWidth: Function) => void;
+export declare const onHoverClick: (pickParams: pickParams, getRouteColor: Function, getRouteWidth: Function, iconDesignations: IconDesignation[]) => void;
 export declare const checkClickedObjectToBeRemoved: (movedData: MovedData[], clickedObject: ClickedObject[], routePaths: RoutePaths[], actions: typeof Actions) => void;
 export declare const defaultMapStateToProps: <P>(state: P) => P;
 export declare const connectToHarmowareVis: (App: any, moreActions?: any, mapStateToProps?: <P>(state: P) => P) => any;
