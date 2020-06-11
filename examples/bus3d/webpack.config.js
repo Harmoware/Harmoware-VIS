659ec91d9df9c9053512fd60cf4ae7f2f530e0fe
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-06-11 16:41:54 +0900

    webpack.config modification

--- examples/bus3d/webpack.config.js
+++ examples/bus3d/webpack.config.js
@@ -56,8 +56,7 @@ module.exports = {
       test: /\.css$/,
       use: [
         'style-loader', // creates style nodes from JS strings
-        'css-loader', // translates CSS into CommonJS
-        'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        'css-loader' // translates CSS into CommonJS
       ]
     }
     ]
