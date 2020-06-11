1aa19601b1475895851c4dc1898bdceec667f35c
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-06-11 14:51:10 +0900

    build:lib

--- lib/src/library/index.d.ts
+++ lib/src/library/index.d.ts
@@ -1,8 +1,7 @@
-import * as Actions from '../actions';
-import { AnalyzedBaseData, InnerProps, RoutePaths, IconDesignation, MovesbaseFile, Movesbase, MovedData, DepotsData, ClickedObject, EventInfo } from '../types';
+import { ActionTypes, AnalyzedBaseData, InnerProps, RoutePaths, IconDesignation, MovesbaseFile, Movesbase, MovedData, DepotsData, ClickedObject, EventInfo } from '../types';
 export declare const getContainerProp: <P>(state: P) => P;
 export declare const calcLoopTime: (timeLength: number, secperhour: number) => number;
-export declare const analyzeMovesBase: (inputData: MovesbaseFile | Movesbase[]) => AnalyzedBaseData;
+export declare const analyzeMovesBase: (inputData: (Movesbase[] | MovesbaseFile)) => AnalyzedBaseData;
 export declare const getDepots: (props: InnerProps) => DepotsData[];
 export declare const getMoveObjects: (props: InnerProps) => MovedData[];
 export interface pickParams {
@@ -10,7 +9,7 @@ export interface pickParams {
     info: EventInfo;
 }
 export declare const onHoverClick: (pickParams: pickParams, getRouteColor: Function, getRouteWidth: Function, iconDesignations: IconDesignation[]) => void;
-export declare const checkClickedObjectToBeRemoved: (movedData: MovedData[], clickedObject: ClickedObject[], routePaths: RoutePaths[], actions: typeof Actions) => void;
+export declare const checkClickedObjectToBeRemoved: (movedData: MovedData[], clickedObject: null | ClickedObject[], routePaths: RoutePaths[], actions: ActionTypes) => void;
 export declare const defaultMapStateToProps: <P>(state: P) => P;
 export declare const connectToHarmowareVis: (App: any, moreActions?: any, mapStateToProps?: <P>(state: P) => P) => import("react-redux").ConnectedComponent<any, Pick<unknown, never>>;
 export declare const getCombinedReducer: (combined?: object) => import("redux").Reducer<import("redux").CombinedState<{
