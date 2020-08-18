307c4030aa978761d96f25fa5a4027ad19070463
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-08-18 09:20:12 +0900

    objLayer

--- examples/visualize-sample/containers/index.tsx
+++ examples/visualize-sample/containers/index.tsx
@@ -147,7 +147,7 @@ class App extends Container<BasedProps, State> {
       return new PointCloudLayer({
         id: 'PointCloudLayer-' + String(idx),
         data,
-        getColor: (x: any) => x.color || [0,255,0,255-x.position[3]*2.55],
+        getColor: (x: any) => x.color || [0,255,0,128+x.position[3]*1.28],
         sizeUnits: 'meters',
         pointSize: 0.1,
       });
