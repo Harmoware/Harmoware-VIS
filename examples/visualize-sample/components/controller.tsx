5f32a184a8248a028424d7f9ed759e355d33f85c
yamauchi <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2020-12-21 09:12:16 +0900

    Compact panel

--- examples/visualize-sample/components/controller.tsx
+++ examples/visualize-sample/components/controller.tsx
@@ -105,6 +105,11 @@ export default class Controller extends React.Component<Props, State> {
     this.props.actions.setIconGradationChange(e.target.checked);
   }
 
+  listExpansion(id: string){
+    let obj=document.getElementById(id).style;
+    obj.display=(obj.display==='none')?'block':'none';
+  }
+
   render() {
     const { settime, timeBegin, timeLength, actions, movedData, movesbase,
       secperhour, animatePause, animateReverse, getMapboxChecked,
@@ -128,6 +133,7 @@ export default class Controller extends React.Component<Props, State> {
                 <div>{movesFileName || '選択されていません'}</div>
               </div>
             </li>
+            <li></li>
             <li>
               <div className="vis_sample_input_button_column">
                 <label htmlFor="DepotsInput" className="btn btn-outline-light btn-sm w-100" title='停留所データ選択'>
@@ -136,6 +142,7 @@ export default class Controller extends React.Component<Props, State> {
                 <div>{depotsFileName || '選択されていません'}</div>
               </div>
             </li>
+            <li></li>
             <li>
               <div className="vis_sample_input_button_column">
                 <label htmlFor="LinemapInput" className="btn btn-outline-light btn-sm w-100" title='ラインマップデータ選択'>
@@ -144,75 +151,86 @@ export default class Controller extends React.Component<Props, State> {
                 <div>{linemapFileName || '選択されていません'}</div>
               </div>
             </li>
+            <li></li>
             <li>
-              <div>
-                <input type="checkbox" id="MapboxChecked" onChange={getMapboxChecked} className="harmovis_input_checkbox" defaultChecked={true} />
-                <label htmlFor="MapboxChecked" className="form-check-label" title='Mapboxマップ表示'>Mapboxマップ表示</label>
-              </div>
+              <span onClick={this.listExpansion.bind(this,'expand1')} >
+                <a style={{'cursor':'pointer'}} >▼ 表示切替スイッチパネル</a>
+              </span>
+              <ul className="list-group">
+                <span id="expand1" style={{'display': 'none','clear': 'both'}}>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="MapboxChecked" onChange={getMapboxChecked} className="harmovis_input_checkbox" defaultChecked={true} />
+                      <label htmlFor="MapboxChecked" className="form-check-label" title='Mapboxマップ表示'>Mapboxマップ表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="MoveDataChecked" onChange={getMoveDataChecked} className="harmovis_input_checkbox" defaultChecked={true} />
+                      <label htmlFor="MoveDataChecked" className="form-check-label" title='運行データ表示'>運行データ表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="IconGradationChecked" onChange={this.seticonGradation.bind(this)} className="harmovis_input_checkbox" />
+                      <label htmlFor="IconGradationChecked" className="form-check-label" title='アイコン色グラデーション'>アイコン色グラデーション</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="IconChangeChecked" onChange={getIconChangeChecked} className="harmovis_input_checkbox" defaultChecked={true} />
+                      <label htmlFor="IconChangeChecked" className="form-check-label" title='アイコン表示パターン切替'>アイコン表示パターン切替</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="form-select" title='３Ｄアイコン表示タイプ切替'>
+                      <label htmlFor="IconCubeTypeSelect" className="form-select-label">３Ｄアイコン表示タイプ切替</label>
+                      <select id="IconCubeTypeSelect" value={iconCubeType} onChange={getIconCubeTypeSelected} >
+                      <option value="0">SimpleMeshLayer</option>
+                      <option value="1">ScenegraphLayer</option>
+                      </select>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="MoveSvgChecked" onChange={getMoveSvgChecked} className="harmovis_input_checkbox" />
+                      <label htmlFor="MoveSvgChecked" className="form-check-label" title='運行データSVG表示'>運行データSVG表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="MoveOptionChecked" onChange={getMoveOptionChecked} className="harmovis_input_checkbox" />
+                      <label htmlFor="MoveOptionChecked" className="form-check-label" title='運行データグラフ表示'>運行データグラフ表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="MoveOptionArcChecked" onChange={getMoveOptionArcChecked} className="harmovis_input_checkbox" />
+                      <label htmlFor="MoveOptionArcChecked" className="form-check-label" title='運行データアーチ表示'>運行データアーチ表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="DepotOptionChecked" onChange={getDepotOptionChecked} className="harmovis_input_checkbox" />
+                      <label htmlFor="DepotOptionChecked" className="form-check-label" title='停留所データオプション表示'>停留所データオプション表示</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="OptionChangeChecked" onChange={getOptionChangeChecked} className="harmovis_input_checkbox" />
+                      <label htmlFor="OptionChangeChecked" className="form-check-label" title='オプション表示パターン切替'>オプション表示パターン切替</label>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <input type="checkbox" id="HeatmapVisible" onChange={getHeatmapVisible} className="harmovis_input_checkbox" />
+                      <label htmlFor="HeatmapVisible" className="form-check-label" title='ヒートマップ表示'>ヒートマップ表示</label>
+                    </div>
+                  </li>
+                </span>
+              </ul>
             </li>
-            <li>
-              <div>
-                <input type="checkbox" id="MoveDataChecked" onChange={getMoveDataChecked} className="harmovis_input_checkbox" defaultChecked={true} />
-                <label htmlFor="MoveDataChecked" className="form-check-label" title='運行データ表示'>運行データ表示</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="IconGradationChecked" onChange={this.seticonGradation.bind(this)} className="harmovis_input_checkbox" />
-                <label htmlFor="IconGradationChecked" className="form-check-label" title='アイコン色グラデーション'>アイコン色グラデーション</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="IconChangeChecked" onChange={getIconChangeChecked} className="harmovis_input_checkbox" defaultChecked={true} />
-                <label htmlFor="IconChangeChecked" className="form-check-label" title='アイコン表示パターン切替'>アイコン表示パターン切替</label>
-              </div>
-            </li>
-            <li>
-              <div className="form-select" title='３Ｄアイコン表示タイプ切替'>
-                <label htmlFor="IconCubeTypeSelect" className="form-select-label">３Ｄアイコン表示タイプ切替</label>
-                <select id="IconCubeTypeSelect" value={iconCubeType} onChange={getIconCubeTypeSelected} >
-                <option value="0">SimpleMeshLayer</option>
-                <option value="1">ScenegraphLayer</option>
-                </select>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="MoveSvgChecked" onChange={getMoveSvgChecked} className="harmovis_input_checkbox" />
-                <label htmlFor="MoveSvgChecked" className="form-check-label" title='運行データSVG表示'>運行データSVG表示</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="MoveOptionChecked" onChange={getMoveOptionChecked} className="harmovis_input_checkbox" />
-                <label htmlFor="MoveOptionChecked" className="form-check-label" title='運行データグラフ表示'>運行データグラフ表示</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="MoveOptionArcChecked" onChange={getMoveOptionArcChecked} className="harmovis_input_checkbox" />
-                <label htmlFor="MoveOptionArcChecked" className="form-check-label" title='運行データアーチ表示'>運行データアーチ表示</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="DepotOptionChecked" onChange={getDepotOptionChecked} className="harmovis_input_checkbox" />
-                <label htmlFor="DepotOptionChecked" className="form-check-label" title='停留所データオプション表示'>停留所データオプション表示</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="OptionChangeChecked" onChange={getOptionChangeChecked} className="harmovis_input_checkbox" />
-                <label htmlFor="OptionChangeChecked" className="form-check-label" title='オプション表示パターン切替'>オプション表示パターン切替</label>
-              </div>
-            </li>
-            <li>
-              <div>
-                <input type="checkbox" id="HeatmapVisible" onChange={getHeatmapVisible} className="harmovis_input_checkbox" />
-                <label htmlFor="HeatmapVisible" className="form-check-label" title='ヒートマップ表示'>ヒートマップ表示</label>
-              </div>
-            </li>
+            <li></li>
             <li><span>ナビゲーションパネル</span>
               <div className="btn-group d-flex" role="group">
                 <NavigationButton buttonType="zoom-in" actions={actions} viewport={viewport} className="btn btn-outline-light btn-sm w-100" />
@@ -220,6 +238,7 @@ export default class Controller extends React.Component<Props, State> {
                 <NavigationButton buttonType="compass" actions={actions} viewport={viewport} className="btn btn-outline-light btn-sm w-100" />
               </div>
             </li>
+            <li></li>
             <li><span>コントロールパネル</span>
               <div className="btn-group d-flex" role="group">
                 {animatePause ?
@@ -240,15 +259,18 @@ export default class Controller extends React.Component<Props, State> {
                 <AddMinutesButton addMinutes={10} actions={actions} className="btn btn-outline-light btn-sm w-100" />
               </div>
             </li>
+            <li></li>
             <li>
               再現中日時&nbsp;<SimulationDateTime settime={settime} />
             </li>
+            <li></li>
             <li title={`${movedData.length} / ${movesbase.length}`}>
               移動体（表示数/総数）&nbsp;{movedData.length}&nbsp;/&nbsp;{movesbase.length}
             </li>
+            <li></li>
             <li>
               <label htmlFor="ElapsedTimeRange">経過時間
-              <ElapsedTimeValue settime={settime} timeBegin={timeBegin} timeLength={timeLength} actions={actions} />&nbsp;／&nbsp;
+              <ElapsedTimeValue settime={settime} timeBegin={timeBegin} timeLength={timeLength} actions={actions} />&nbsp;/&nbsp;
               <input type="number" value={timeLength} onChange={e=>actions.setTimeLength(+e.target.value)} className="harmovis_input_number" min={0} max={timeLength} />&nbsp;秒
               </label>
               <ElapsedTimeRange settime={settime} timeLength={timeLength} timeBegin={timeBegin} actions={actions} id="ElapsedTimeRange" />
@@ -257,34 +279,41 @@ export default class Controller extends React.Component<Props, State> {
               <label htmlFor="SpeedRange">スピード<SpeedValue secperhour={secperhour} actions={actions} />秒/時</label>
               <SpeedRange secperhour={secperhour} actions={actions} id="SpeedRange" />
             </li>
-            <li><div>経路操作</div>
-              <div className="btn-group d-flex" role="group">
-                <button onClick={this.saveRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='SAVE'>
-                  <span className="button_span"><Icon icon={icSave} />&nbsp;SAVE&nbsp;
-                    <span className="badge badge-light">{saveRouteGroup.length}</span>
-                  </span>
-                </button>
-                <button onClick={this.displayRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='DISPLAY'>
-                  <span className="button_span"><Icon icon={icLayers} />&nbsp;DISPLAY&nbsp;
-                    <span className="badge badge-light">{routeGroupDisplay ? displayIndex : 0}</span>
-                  </span>
-                </button>
-              </div>
-              <div className="btn-group d-flex" role="group">
-                <button onClick={this.clearAllRoute.bind(this)} className="btn btn-outline-light btn-sm w-100" title='All Clear'>
-                  <span className="button_span"><Icon icon={icDeleteForever} />&nbsp;All Clear</span>
-                </button>
-                <button onClick={this.deleteRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='DELETE'>
-                  <span className="button_span"><Icon icon={icDelete} />&nbsp;DELETE</span>
-                </button>
-              </div>
-            </li>
+            <li></li>
             <li>
               開始 UNIX TIME 設定&nbsp;<input type="number" value={timeBegin} onChange={e=>actions.setTimeBegin(+e.target.value)} className="harmovis_input_number" />
             </li>
             <li>
               開始 日付&nbsp;{(new Date(timeBegin * 1000)).toLocaleString('ja-JP',
                 {year: 'numeric',month: '2-digit',day: '2-digit',hour: '2-digit',minute: '2-digit',second: '2-digit',weekday: 'short' })}
+            </li>
+            <li></li>
+            <li>
+              <span onClick={this.listExpansion.bind(this,'expand2')} >
+                <a style={{'cursor':'pointer'}} >▼ 経路線操作パネル</a>
+              </span>
+              <span id="expand2" style={{'display': 'none','clear': 'both'}}>
+                <div className="btn-group d-flex" role="group">
+                  <button onClick={this.saveRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='SAVE'>
+                    <span className="button_span"><Icon icon={icSave} />&nbsp;SAVE&nbsp;
+                      <span className="badge badge-light">{saveRouteGroup.length}</span>
+                    </span>
+                  </button>
+                  <button onClick={this.displayRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='DISPLAY'>
+                    <span className="button_span"><Icon icon={icLayers} />&nbsp;DISPLAY&nbsp;
+                      <span className="badge badge-light">{routeGroupDisplay ? displayIndex : 0}</span>
+                    </span>
+                  </button>
+                </div>
+                <div className="btn-group d-flex" role="group">
+                  <button onClick={this.clearAllRoute.bind(this)} className="btn btn-outline-light btn-sm w-100" title='All Clear'>
+                    <span className="button_span"><Icon icon={icDeleteForever} />&nbsp;All Clear</span>
+                  </button>
+                  <button onClick={this.deleteRouteGroup.bind(this)} className="btn btn-outline-light btn-sm w-100" title='DELETE'>
+                    <span className="button_span"><Icon icon={icDelete} />&nbsp;DELETE</span>
+                  </button>
+                </div>
+              </span>
             </li>
           </ul>
         </div>
