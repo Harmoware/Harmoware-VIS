60b85981a3c3db28468fb700e43a82bb52618fd7
Yamauchi Yoshitaka <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2021-06-15 08:28:43 +0900

    Remove unwanted typescript definitions

--- global.d.ts
+++ global.d.ts
@@ -1,16 +1,3 @@
 declare module "@loaders.gl/core" ;
 declare module "@loaders.gl/gltf" ;
 declare module "@loaders.gl/obj" ;
-declare module "react-icons-kit/md" {
-  export const ic_fast_forward: object;
-  export const ic_fast_rewind: object;
-  export const ic_forward: object;
-  export const ic_pause_circle_outline: object;
-  export const ic_play_circle_outline: object;
-  export const ic_replay: object;
-  export const ic_navigation: object;
-  export const ic_delete_forever: object;
-  export const ic_save: object;
-  export const ic_layers: object;
-  export const ic_delete: object;
-}
--- luma.gl.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "@luma.gl/engine" ;
